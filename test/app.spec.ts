0f24308fc093b42d89702d3a605d9d0a38138b4b
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-03-01 15:53:21 +0200

    feat: added tests

---
+++ test/app.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import supatest from "supertest"
+import { describe, beforeEach, it, expect } from 'vitest'
+
+describe('AppController (e2e)', () => {
+	let app: INestApplication;
+
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
+
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
+
+	it('/ (GET)', async () => {
+		const response = await supatest(app.getHttpServer()).get('/').expect(200);
+		expect(response.text).toBe('Hello!');
+	});
+});
